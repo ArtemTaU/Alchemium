c5a809b4a7c9e97894fd0b3650c8bf73fcea1fae
ArtemTaU <tikhanovskii@phystech.edu>
2025-07-25 20:17:02 +0300

    feature: validator for update added

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -1,5 +1,7 @@
 from src.exceptions import RepositoryUsageError
 
+from ..typing import T
+
 
 def validate_model_defined(cls):
     """
@@ -11,4 +13,18 @@ def validate_model_defined(cls):
     if getattr(cls, "model", None) is None:
         raise RepositoryUsageError(
             details=f"{cls.__name__} repository must define model attribute"
+        )
+
+
+def validate_object_to_update_defined(cls, obj: T):
+    """
+    Ensure that the model attribute is defined on the class.
+
+    :param cls: The class (repository or mixin) being validated.
+    :param obj: ORM model instance to update.
+    :raises RepositoryUsageError: If the model attribute is None.
+    """
+    if obj is None:
+        raise RepositoryUsageError(
+            details=f"{cls.__name__} Object to update must be provided"
         )

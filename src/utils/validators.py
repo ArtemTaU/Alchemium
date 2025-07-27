e6231dc58e22e7a52a453b905d9e70d0b7aaae38
ArtemTaU <tikhanovskii@phystech.edu>
2025-07-27 23:46:33 +0300

    feature: added object instance validator

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -28,3 +28,19 @@ def validate_object_to_update_defined(cls, obj: T):
         raise RepositoryUsageError(
             details=f"{cls.__name__} Object to update must be provided"
         )
+
+
+def validate_object_instance(cls, obj: T):
+    """
+    Ensure that the provided object is an instance of the repository's model.
+
+    :param cls: The class (repository or mixin) being validated.
+    :param obj: The object to check.
+    :raises RepositoryUsageError: If the object is not an instance of cls.model.
+    """
+    model = getattr(cls, "model", None)
+
+    if not isinstance(obj, model):
+        raise RepositoryUsageError(
+            details=f"Expected instance of '{model.__name__}', got '{type(obj).__name__}'"
+        )
